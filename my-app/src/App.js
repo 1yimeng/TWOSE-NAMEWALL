a4ee04e880a8cebf904f939861ee47b3c2ba196f
1yimeng <yimwang6@gmail.com>
2023-01-06 16:20:08 -0700

    format

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo1.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>University of Alberta Computer Engineering Club</h2>
+        <h3> Leave your name here by typing in some code! </h3>
+        <a style={{color: 'Pink'}}>yi meng was here</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
