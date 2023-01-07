40ff04b95a87212702b9928718b584d159d05392
1yimeng <yimwang6@gmail.com>
2023-01-07 11:28:44 -0700

    names

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,8 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>University of Alberta Computer Engineering Club</h2>
-        <h3> Leave your name here by typing in some code! </h3>
+        <h3> Leave your name here by typing in some code!! </h3>
         <a style={{color: 'Pink'}}>yi meng was here</a>
+        <a>luca</a>
+        <a>massimo</a>
+        <a>Violet</a>
       </header>
     </div>
   );
